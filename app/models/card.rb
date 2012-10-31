4245829861a5f8e0ddfce3b73955bb83a137d390
Néville Dubuis <ndubuis@gmail.com>
2012-10-31 20:16:28 +0100

    add validators

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,4 +3,9 @@ class Card < ActiveRecord::Base
   belongs_to :type
 
   attr_accessible :name, :type_id, :user_id, :object_id 
+
+  validates :name, :presence => true
+  validates :type_id, :presence => true
+  validates :user_id, :presence => true
+  validates :object_id, :presence => true
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,5 +1,8 @@
 class Type < ActiveRecord::Base
   has_many :cards
 
-  attr_accessible :id, :name
+  attr_accessible :name, :classname
+
+  validates :name, :presence => true
+  validates :classname, :presence => true
 end
